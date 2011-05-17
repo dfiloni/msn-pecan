4e99dce6583e069d40013117acc2ab6bac543727
Devid Antonio Filoni <devidfil@gmail.com>
2011-05-17 22:30:37 +0200

    pn_global.h: trivial reorganization Signed-off-by: Devid Antonio Filoni <devidfil@gmail.com>

--- pn_global.h
+++ pn_global.h
@@ -23,40 +23,44 @@
 #define PN_BUF_LEN 8192
 #define PN_MAX_MESSAGE_LENGTH 1564
 
-#define BUDDY_ALIAS_MAXLEN 387 /** @todo why is this needed? */
-
 typedef enum {
-    PN_CLIENT_CAP_WIN_MOBILE = 0x000001,
-    PN_CLIENT_CAP_UNKNOWN_1 = 0x000002,
-    PN_CLIENT_CAP_INK_GIF = 0x000004,
-    PN_CLIENT_CAP_INK_ISF = 0x000008,
-    PN_CLIENT_CAP_VIDEO_CHAT = 0x000010,
-    PN_CLIENT_CAP_BASE = 0x000020,
-    PN_CLIENT_CAP_MSNMOBILE = 0x000040,
-    PN_CLIENT_CAP_MSNDIRECT = 0x000080,
-    /* PN_CLIENT_CAP_WEBMSGR = 0x000200, */
-    /* 0x000200 doesn't work with WLM >= 8.5 */
-    PN_CLIENT_CAP_TGW = 0x000800,
-    PN_CLIENT_CAP_SPACE = 0x001000,
-    PN_CLIENT_CAP_MCE = 0x002000,
-    PN_CLIENT_CAP_DIRECTIM = 0x004000,
-    PN_CLIENT_CAP_WINKS = 0x008000,
-    PN_CLIENT_CAP_SEARCH = 0x010000,
-    /* PN_CLIENT_CAP_BOT = 0x020000, */
-    /* 0x020000 doesn't work with WLM >= 8.5 */
-    PN_CLIENT_CAP_VOICE_CLIP = 0x040000,
-    PN_CLIENT_CAP_SCHANNEL = 0x080000,
-    PN_CLIENT_CAP_SIP_INVITE = 0x100000,
-    PN_CLIENT_CAP_SDRIVE = 0x400000,
+    PN_CLIENT_CAP_WIN_MOBILE = 0x01, /* CapabilityMobileOnline */
+    PN_CLIENT_CAP_MSN8USER = 0x02, /* CapabilityMSN8User */
+    PN_CLIENT_CAP_INK_GIF = 0x04, /* CapabilityRendersGif */
+    PN_CLIENT_CAP_INK_ISF = 0x08, /* CapabilityRendersIsf */
+    PN_CLIENT_CAP_VIDEO_CHAT = 0x10, /* CapabilityWebCamDetected */
+    PN_CLIENT_CAP_BASE = 0x20, /* CapabilitySupportsChunking */
+    PN_CLIENT_CAP_MSNMOBILE = 0x40, /* CapabilityMobileEnabled */
+    PN_CLIENT_CAP_MSNDIRECT = 0x80, /* CapabilityDirectDevice */
+    PN_CLIENT_CAP_WEBMSGR = 0x200, /* CapabilityWebIMClient */
+    PN_CLIENT_CAP_TGW = 0x800, /* CapabilityConnectedViaTGW */
+    PN_CLIENT_CAP_SPACE = 0x1000, /* CapabilityHasSpace */
+    PN_CLIENT_CAP_MCE = 0x2000, /* CapabilityMCEUser */
+    PN_CLIENT_CAP_DIRECTIM = 0x4000, /* CapabilitySupportsDirectIM */
+    PN_CLIENT_CAP_WINKS = 0x8000, /* CapabilitySupportsWinks */
+    PN_CLIENT_CAP_SEARCH = 0x10000,
+    PN_CLIENT_CAP_BOT = 0x20000, /* CapabilityIsBot */
+    PN_CLIENT_CAP_VOICE_CLIP = 0x40000, /* CapabilitySupportsVoiceIM */
+    PN_CLIENT_CAP_SCHANNEL = 0x80000, /* CapabilitySupportsSChannel */
+    PN_CLIENT_CAP_SIP_INVITE = 0x100000, /* CapabilitySupportsSipInvite */
+    PN_CLIENT_CAP_SDRIVE = 0x400000, /* CapabilitySupportsSDrive */
+    PN_CLIENT_CAP_ONECARE = 0x1000000, /* CapabilityHasOnecare */
+    PN_CLIENT_CAP_P2P_TURN = 0x2000000, /* CapabilityP2PSupportsTurn */
+    PN_CLIENT_CAP_P2P_BOOTSTRAP = 0x4000000, /* CapabilityP2PBootstrapViaUUN */
 } PnClientCaps;
 
 typedef enum {
-    PN_CLIENT_VER_5_0 = 0x00,
-    PN_CLIENT_VER_6_0 = 0x10,
-    PN_CLIENT_VER_6_1 = 0x20,
-    PN_CLIENT_VER_6_2 = 0x30,
-    PN_CLIENT_VER_7_0 = 0x40,
-    PN_CLIENT_VER_7_5 = 0x50,
+    PN_CLIENT_VER_6_0 = 0x10000000, /* CapabilityMsgrVersion1 */
+    PN_CLIENT_VER_6_1 = 0x20000000, /* CapabilityMsgrVersion2 */
+    PN_CLIENT_VER_6_2 = 0x30000000, /* CapabilityMsgrVersion3 */
+    PN_CLIENT_VER_7_0 = 0x40000000, /* CapabilityMsgrVersion4 */
+    PN_CLIENT_VER_7_5 = 0x50000000, /* CapabilityMsgrVersion5 */
+    PN_CLIENT_VER_8_0 = 0x60000000, /* CapabilityMsgrVersion6 */
+    PN_CLIENT_VER_8_1 = 0x70000000, /* CapabilityMsgrVersion7 */
+    PN_CLIENT_VER_8_5 = 0x80000000, /* CapabilityMsgrVersion8 */
+    PN_CLIENT_VER_9_0 = 0x90000000, /* CapabilityMsgrVersion9 */
+    PN_CLIENT_VER_2009 = 0xA0000000, /* CapabilityMsgrVersion10 */
+    PN_CLIENT_VER_2011 = 0xB0000000, /* CapabilityMsgrVersion11 */
 } PnClientVerId;
 
 #endif /* PN_GLOBAL_H */
